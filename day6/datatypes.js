37f539a4dba062bdb0cb89ba253cf0b614a2c3e1
PrepInstaPrime <gaurav.pandey@adda247.com>
2025-07-25 16:42:09 +0530

    day6

---
+++ day6/datatypes.js
@@ -0,0 +1,15 @@
+let a=5;
+console.log("type of a",typeof a)
+let b=null
+console.log("type of a",typeof b)
+let c= true;
+console.log("type of a",typeof c)
+let d= BigInt(45678956785678);
+let e= 5n;
+console.log("type of a",typeof d, typeof e)
+let f="Gaurav Pandey"
+console.log("type of a",typeof f)
+let g= Symbol("hy");
+console.log("type of a",typeof g)
+let h;
+console.log("type of a",typeof h)
---
+++ day6/intro.js
@@ -0,0 +1,16 @@
+console.log("hello world");
+var a=5;
+console.log("Value of a: ",a)
+var a=6;
+console.log("Value of a: ",a)
+let b=7;
+console.log("Value of b: ",b)
+// let b=8; // error
+b=8;
+console.log("Value of b: ",b)
+
+const c=9;
+console.log("Value of b: ",c)
+c=7;
+console.log("Value of b: ",c)
+
